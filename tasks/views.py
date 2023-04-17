a71c1f2e65a1a7c4049d273b782ba8b8453f9349
Abdulaziz Sadi <abdullkitindi@gmail.com>
2023-04-17 23:29:22 +0300

    added session to logic to associate with user data

--- tasks/views.py
+++ tasks/views.py
@@ -4,11 +4,14 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 
 # Create your views here.
-tasks = []
+
 
 
 def index(request):
-    return render(request, 'index.html', {'tasks': tasks})
+    if "tasks" not in request.session:
+        request.session["tasks"] =[]
+        
+    return render(request, 'index.html',{'tasks': request.session['tasks']} )
 
 def add(request):
     
@@ -18,7 +21,7 @@ def add(request):
             error_message = 'Add task field'
             return render(request, 'add.html' , {'error_message': error_message})
         else:
-            tasks.append(task)
+            request.session["tasks"]+=[task]
             return HttpResponseRedirect(reverse('tasks:index'))
     
     return render(request, 'add.html' )
