a2f42139bf467ec48666eadb11f5742aeb9f8f3e
Abdulaziz Sadi <abdullkitindi@gmail.com>
2023-04-17 22:10:20 +0300

    added the task from

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,20 @@
+
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+tasks = []
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'tasks': tasks})
 
 def add(request):
-    return render(request, 'add.html')
+    
+    if request.method == 'POST':
+        task = request.POST['task']
+        tasks.append(task)
+        return HttpResponseRedirect(reverse('tasks:index'))
+    
+    return render(request, 'add.html' )
