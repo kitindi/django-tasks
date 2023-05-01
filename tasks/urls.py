5d39229d9eeaca3781d2188f22295c8097730997
kitindi <abdullkitindi@gmail.com>
2023-05-01 10:26:34 +0300

    added edit page and corresponding routing logic

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,5 +6,7 @@ app_name = 'tasks'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add/', views.add, name='add')
+    path('add/', views.add, name='add'),
+    path('edit/<int:id>/', views.edit, name='edit'),
+    path('delete/<int:id>/', views.delete, name='delete'),    
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -24,3 +24,20 @@ def add(request):
             return redirect('/')
     
     return render(request, 'add.html', context)
+
+def delete(request, id):
+    task = Task.objects.get(id=id)
+    task.delete()
+    return redirect('/')
+
+def edit(request,id):
+    task = Task.objects.get(id=id)
+    form = TaskCreationForm(instance=task)
+    context ={'form':form}
+    if request.method == 'POST':
+        form = TaskCreationForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        
+    return render(request, 'edit.html', {'form': form})
