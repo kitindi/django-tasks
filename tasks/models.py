bd05445940a4943b5ca8f9bd9cfe703242191d4d
kitindi <abdullkitindi@gmail.com>
2023-05-07 03:21:05 +0300

    added user based feeds

--- tasks/models.py
+++ tasks/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Task(models.Model):
     title = models.CharField(max_length=100)
-    # created_by = models.ForeignKey(User, related_name='tasks',on_delete=models.CASCADE)
-    # updated_by = models.ForeignKey(User, null=True, related_name='+',on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
--- tasks/views.py
+++ tasks/views.py
@@ -2,48 +2,57 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .forms import TaskCreationForm
 from .models import Task
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
 
-@login_required(login_url='/signin/')
+
+@login_required
 def profile(request):
-    tasks = Task.objects.all()
-    context = {'tasks':tasks}
-        
-    return render(request, 'profile.html', context)
+    tasks = Task.objects.filter(author=request.user)
+    context = {"tasks": tasks, "user": request.user.username}
 
-@login_required(login_url='/signin/')
+    return render(request, "profile.html", context)
+
+
+@login_required
 def add(request):
     form = TaskCreationForm()
-    context ={'form':form}
+    context = {"form": form}
 
-    if request.method == 'POST':
-        form = TaskCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/profile/')
-    
-    return render(request, 'add.html', context)
+    if request.method == "POST":
+        task_form = TaskCreationForm(request.POST)
+        if task_form.is_valid():
+            # add user to the instance ↓
+            # task_form = form.save(commit=False)
+            task_form.instance.author = request.user
+            task_form.save()
 
-@login_required(login_url='/signin/')
+            return redirect("/profile/")
+
+    return render(request, "add.html", context)
+
+
+@login_required
 def delete(request, id):
     task = Task.objects.get(id=id)
     task.delete()
-    return redirect('/profile/')
+    return redirect("/profile/")
 
-@login_required(login_url='signin/')
-def edit(request,id):
+
+@login_required
+def edit(request, id):
     task = Task.objects.get(id=id)
     form = TaskCreationForm(instance=task)
-    context ={'form':form}
-    if request.method == 'POST':
+    context = {"form": form}
+    if request.method == "POST":
         form = TaskCreationForm(request.POST, instance=task)
         if form.is_valid():
             form.save()
-            return redirect('/profile/')
-        
-    return render(request, 'edit.html', {'form': form})
+            return redirect("/profile/")
+
+    return render(request, "edit.html", {"form": form})
